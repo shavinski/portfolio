23e16056adfdf1b0ce67a82660be2339ec343447
Jakob <shavinski.jakob@gmail.com>
2023-06-24 17:09:10 -0700

    Finish Home component

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
 import Home from './Components/Home';
+import AboutMe from './Components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
+      <AboutMe />
     </div>
   );
 }
---
+++ personal-portfolio/src/Components/AboutMe.js
@@ -0,0 +1,12 @@
+import './AboutMe.css'
+
+function AboutMe() {
+
+    return (
+        <section className='aboutme' id='aboutme'>
+            <h1>Hello</h1>
+        </section>
+    )
+}
+
+export default AboutMe
