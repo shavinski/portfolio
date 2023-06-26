3e137d3cea5dd2a343cf536cfa3547654f3e447f
Jakob <shavinski.jakob@gmail.com>
2023-06-26 11:50:07 -0700

    refactor home page

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,23 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
 import Home from './Components/Home';
+import AboutMe from './Components/AboutMe';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-<<<<<<< HEAD
-      {/* <Home />
-      <AboutMe /> */}
-=======
-<<<<<<< HEAD
+      {/* <NavBar /> */}
       <Home />
-      {/* <AboutMe /> */}
-=======
-      {/* <Home />
-      <AboutMe /> */}
->>>>>>> af95267dfad398b90ededa5d857ba1d274182a6e
->>>>>>> parent of 0a801c4 (some slight change)
+      <AboutMe /> 
     </div>
   );
 }
---
+++ personal-portfolio/src/Components/AboutMe.js
@@ -0,0 +1,20 @@
+import './AboutMe.css'
+
+function AboutMe() {
+    return (
+        <section className='aboutme' id='aboutme'>
+            <div className='container'>
+                <div className='aboutme-title'>
+                    <h1>About Me</h1>
+                </div>
+
+                <div>
+                    
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default AboutMe
