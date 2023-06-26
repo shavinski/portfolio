4aa334da9dd45c1a987b67fd0c338c0f0c5e6fc2
Jakob <shavinski.jakob@gmail.com>
2023-06-26 10:00:38 -0700

    revert changes

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
 import Home from './Components/Home';
-import AboutMe from './Components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+<<<<<<< HEAD
       {/* <Home />
       <AboutMe /> */}
+=======
+      <Home />
+>>>>>>> parent of 23e1605 (Finish Home component)
     </div>
   );
 }
