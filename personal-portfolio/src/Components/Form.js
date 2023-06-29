fba7d22ed19ba8d9a605bae8d5edebcaf795c1c4
Jakob <shavinski.jakob@gmail.com>
2023-06-29 15:51:27 -0700

    styled contact form so more

--- personal-portfolio/src/Components/Form.js
+++ personal-portfolio/src/Components/Form.js
@@ -1,27 +1,66 @@
+import { useState } from 'react';
 import './Form.css'
 
 function Form() {
+    const startingData = {
+        name: '',
+        email: '',
+        message: '',
+    }
+    const [formData, setFormData] = useState(startingData)
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+      };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+    }
+
     return (
         <section className="contact" id="contact">
             <div className='container-contact'>
                 <div className='connect-title title'>Connect with Me</div>
                 <form>
                     <div className='form-field'>
-                        <label>Name</label>
-                        <input></input>
+                        <label className='form-label' type='text' id='name'>Name</label>
+                        <input 
+                        className='form-input'
+                        type='text' 
+                        value={startingData.name} 
+                        onChange={handleChange}
+                        placeholder='Your name'></input>
                     </div>
 
                     <div className='form-field'>
-                        <label>Email</label>
-                        <input></input>
+                        <label className='form-label' type='text' id='name'>Email</label>
+                        <input 
+                        className='form-input'
+                        type='text' 
+                        value={startingData.name} 
+                        onChange={handleChange}
+                        placeholder='Your email'></input>
                     </div>
 
                     <div className='form-field'>
-                        <label>Message</label>
-                        <textarea></textarea>
+                        <label className='form-label' type='text' id='name'>Message</label>
+                        <textarea 
+                        className='form-input'
+                        type='text' 
+                        value={startingData.name} 
+                        onChange={handleChange}
+                        ></textarea>
                     </div>
 
-                    <button type='submit'>Submit</button>
+                    <button 
+                    type='click' 
+                    onClick={handleSubmit}>
+                    Submit</button>
                 </form>
             </div>
         </section>
